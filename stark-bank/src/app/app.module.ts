871c0a4fe9534cfaf326ef8ba073e238c16435d6
charan <charanreddyanumula@gmail.com>
2019-08-17 11:56:19 +0530

    routerModule added

--- stark-bank/src/app/app.module.ts
+++ stark-bank/src/app/app.module.ts
@@ -1,10 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
+
+import {FormsModule} from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BankUserComponent } from './bank-user/bank-user.component';
 import { CustomerListComponent } from './customer-list/customer-list.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
@@ -14,7 +17,22 @@ import { CustomerListComponent } from './customer-list/customer-list.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path:'',
+        component:AppComponent
+      },
+      {
+        path:'bankUser',
+        component:BankUserComponent
+      },
+      {
+        path:'customerList',
+        component:CustomerListComponent
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
