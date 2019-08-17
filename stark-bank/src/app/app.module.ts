278574091cfe9ae75fca41c2d33534f8955f9a87
SandhyaLingamalla <53751270+SandhyaLingamalla@users.noreply.github.com>
2019-08-17 12:27:43 +0530

    added reistration and header

--- stark-bank/src/app/app.module.ts
+++ stark-bank/src/app/app.module.ts
@@ -8,22 +8,23 @@ import { AppComponent } from './app.component';
 import { BankUserComponent } from './bank-user/bank-user.component';
 import { CustomerListComponent } from './customer-list/customer-list.component';
 import { RouterModule } from '@angular/router';
+import { RegisterComponent } from './register/register.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BankUserComponent,
-    CustomerListComponent
+    CustomerListComponent,
+    RegisterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
     RouterModule.forRoot([
-      {
-        path:'',
-        component:AppComponent
-      },
+      
       {
         path:'bankUser',
         component:BankUserComponent
@@ -31,6 +32,9 @@ import { RouterModule } from '@angular/router';
       {
         path:'customerList',
         component:CustomerListComponent
+      },{
+        path:'register',
+        component:RegisterComponent
       }
     ])
   ],
