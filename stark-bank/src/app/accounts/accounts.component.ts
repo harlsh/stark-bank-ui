5279476422c7c43d250dde28e7a27890a6cf3176
charan <charanreddyanumula@gmail.com>
2019-08-21 12:05:22 +0530

    added accounts

--- stark-bank/src/app/accounts/accounts.component.ts
+++ stark-bank/src/app/accounts/accounts.component.ts
@@ -8,6 +8,23 @@ import { Component, OnInit } from '@angular/core';
 export class AccountsComponent implements OnInit {
 
   constructor() { }
+  accounts=[{
+    id:1,
+    accountNumber:101,
+    accountName:"SBI",
+    balance:1016
+  },{
+    id:2,
+    accountNumber:102,
+    accountName:"SBI",
+    balance:505
+  },{
+    id:3,
+    accountNumber:103,
+    accountName:"SBI",
+    balance:332
+  }
+]
 
   ngOnInit() {
   }
