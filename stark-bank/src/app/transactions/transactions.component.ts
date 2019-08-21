16ed10de60a5609b16dc6329c82afb1ba39cabc3
rachana-kavukuntla <rachanakavukuntla@gmail.com>
2019-08-21 10:36:53 +0530

    transactions added

---
+++ stark-bank/src/app/transactions/transactions.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-transactions',
+  templateUrl: './transactions.component.html',
+  styleUrls: ['./transactions.component.css']
+})
+export class TransactionsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+  attributes = ["ID","TRANSACTIONAMOUNT","DEBITEDACCOUNT","CREDITEDACCOUNT"];
+  transactions = [
+    {
+      id:1,
+      transactionAmount: 20000,
+      creditedAccount: 234563748,
+      debitedAccount:6475894,
+    },
+    {
+      id:2,
+      transactionAmount: 40000,
+      creditedAccount: 234563748,
+      debitedAccount:6475894,
+    },
+    {
+      id:3,
+      transactionAmount: 4560000,
+      creditedAccount: 234563748,
+      debitedAccount:6475894,
+    },
+    {
+      id:4,
+      transactionAmount: 245600,
+      creditedAccount: 234563748,
+      debitedAccount:6475894,
+    },
+    {
+      id:5,
+      transactionAmount: 890000,
+      creditedAccount: 234563748,
+      debitedAccount:6475894,
+    }
+
+    ]
+
+}
