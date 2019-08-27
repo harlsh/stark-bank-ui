c24e339ff3fb9279589089573d3e249ff035e86d
charan <charanreddyanumula@gmail.com>
2019-08-27 10:31:50 +0530

    getCustomerServices added

--- stark-bank/src/app/customer.service.ts
+++ stark-bank/src/app/customer.service.ts
@@ -3,6 +3,7 @@ import { Customer } from './Customer';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { HttpHeaders } from '@angular/common/http';
+import { BankUser } from './BankUser';
 const httpOptions = {
   headers: new HttpHeaders({
     'Content-Type':  'application/json'
@@ -14,14 +15,11 @@ const httpOptions = {
 })
 export class CustomerService {
 
-  private CUSTOMER_API_URL = "http://localhost:8080/api/v1/customers";
+  private CUSTOMER_API_URL = "http://localhost:8080/api/v1/customers/";
   private BRANCH_API_URL = "http://localhost:8080/api/v1/branches/";
-  
 
   branch:any[] = [];
   customers: any[] = [];
-  transactions:any[]=[];
-
   constructor(private httpClient:HttpClient) { }
 
   saveCustomer(customer: Customer, id: string):Observable<any>{
@@ -33,6 +31,10 @@ export class CustomerService {
     return this.httpClient.get<any[]>(this.BRANCH_API_URL);
   }
 
+  loginBankUser(bankUser: BankUser, id: string):Observable<any>{
+    return this.httpClient.post(this.BRANCH_API_URL + id + "/bankusers/login", JSON.stringify(bankUser), httpOptions)
+  }
+  
   getCustomerTransactions(id:any, aid:any):Observable<any[]>{
     return this.httpClient.get<any[]>(this.CUSTOMER_API_URL+id+"/accounts/"+aid+"/transactions");
   }
